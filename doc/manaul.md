8824e587129ebc9ff0cb140a3000d751baabcefa
uniqueding <1224472370@qq.com>
2021-09-21 23:32:07 +0800

    add generate manual

---
+++ doc/manaul.md
@@ -0,0 +1,17 @@
+# manual
+
+## zsh/fun.sh
+
+| key | note | code | pic |
+| --- | ---- | ---- | --- |
+| fzfl | show file list useby fzf |  | ![pic]() |
+| fzflh | show home file list useby fzf | fzfl $HOME | ![pic]() |
+| fzflr | show / file list useby fzf | fzfl / | ![pic]() |
+| fzfp | show ps -ef list useby fzf |  | ![pic]() |
+| fzfc [filetype:optional] | show current directory file lines use by fzf and jump to nvim |  | ![pic]() |
+
+## zsh/alias.sh
+
+| key | note | code | pic |
+| --- | ---- | ---- | --- |
+
---
+++ generator.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import yaml
+import json
+import re
+
+file_list = [
+    'zsh/fun.sh',
+    'zsh/alias.sh',
+        ]
+
+manual_dic = {}
+
+def ReadHead(file_path: str):
+    with open(file_path, 'r') as f:
+        yaml_str = ''
+        manual_dic[file_path] = []
+        is_new = True
+
+        while True:
+            line = f.readline()
+            if line == '':
+                break
+            if re.search('---',line):
+                if is_new:
+                    is_new = False
+                    continue
+
+                print(yaml_str)
+                tmp_dic = yaml.load(yaml_str, Loader=yaml.BaseLoader)
+                manual_dic[file_path].append(tmp_dic)
+                yaml_str = ''
+                is_new = True
+
+            if not is_new:
+                yaml_str += line[1:]
+
+
+def manaul_scan():
+    for file_item in file_list:
+        ReadHead(file_item)
+
+
+def print_json():
+    with open('doc/manaul.json', 'w') as f:
+        json.dump(manual_dic, f, indent=4, separators=(',', ':'))
+
+def print_md():
+    with open('doc/manaul.md', 'w') as f:
+        f.write('# manual\n\n')
+        for key, value in manual_dic.items():
+            f.write('## ' + key + '\n\n')
+            f.write('| key | note | code | pic |\n')
+            f.write('| --- | ---- | ---- | --- |\n')
+            for item in value:
+                f.write(
+                        '| ' + \
+                        item['key'] + ' | ' + \
+                        item['note'] + ' | ' + \
+                        item['code'] + ' | ' + \
+                        '![pic](' + item['pic'] + ')' + ' |\n'
+                        )
+
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    manaul_scan()
+    print_json()
+    print_md()
+    # print(manual_dic)
