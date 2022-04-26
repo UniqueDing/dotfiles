a9c05aa4c7e128657735bd6954867988d402e67d
uniqueding <1224472370@qq.com>
2022-04-27 01:42:41 +0800

    add waybar waydroid

--- sway/script/gesture.sh
+++ sway/script/gesture.sh
@@ -7,12 +7,12 @@ ROTATE=$(( $1 / 90 ))
 lisgd \
     -g "1,LR,L,S,R, wtype -M alt -P left -p left -m alt &" \
     -g "1,RL,R,S,R, wtype -M alt -P left -p left -m alt &" \
-    -g "1,DU,B,S,R, notify-send hhh && $HOME/.config/waybar/script/keyboard.sh toggle &" \
+    -g "1,DU,B,S,R, $HOME/.config/waybar/script/keyboard.sh toggle &" \
     -g "1,UD,T,S,R, nwggrid &" \
     -g "2,LR,*,*,R, swaymsg workspace prev" \
     -g "2,RL,*,*,R, swaymsg workspace next" \
-    -g "3,LR,*,*,R, $HOME/.config/waybar/script/workspace.sh prev &" \
-    -g "3,RL,*,*,R, $HOME/.config/waybar/script/workspace.sh next &" \
+    -g "2,DU,*,*,R, $HOME/.config/waybar/script/workspace.sh prev &" \
+    -g "2,UD,*,*,R, $HOME/.config/waybar/script/workspace.sh next &" \
     -g "3,UD,*,*,R, $HOME/.config/waybar/script/screenshot.sh full &" \
     -g "3,DU,*,*,R, $HOME/.config/waybar/script/screenshot.sh rect &" \
     -m 1200 \
---
+++ waybar/script/waydroid.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $1 == "status" ]];then
+    while true
+    do
+    if [ -f /tmp/waybar_waydroid ];then
+        echo "{\"alt\":\"activated\"}"
+    else
+        echo "{\"alt\":\"deactivated\"}"
+    fi
+    sleep 0.1
+    done
+elif [[ $1 == "toggle" ]];then
+    if [ -f /tmp/waybar_waydroid ];then
+        systemctl stop waydroid-container.service
+        exec ~/.config/sway/lauch.sh
+        swaymsg workspace 1
+        rm -f /tmp/waybar_waydroid
+    else
+        touch /tmp/waybar_waydroid
+        systemctl start waydroid-container.service
+        swaymsg reload
+        swaymsg workspace 0
+        waydroid show-full-ui &
+
+        killall lisgd
+
+        lisgd \
+            -g "3,UD,*,*,R, swaymsg fullscreen" \
+            -d /dev/input/event30 &
+    fi
+fi
