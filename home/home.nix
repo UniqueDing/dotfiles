fdeb293aafaef1052dac5a0e6de0d29109cd795b
dingzhixuan <dingzhixuan@uniontech.com>
2024-10-17 21:04:47 +0800

    add yazi catppuccin

--- home/home.nix
+++ home/home.nix
@@ -15,6 +15,7 @@
   home.activation.linkDotfiles = config.lib.dag.entryAfter ["writeBoundary"]
   ''
     ln -sfn $HOME/dotfiles/home/ranger $HOME/.config/ranger
+    ln -sfn $HOME/dotfiles/home/bat $HOME/.config/bat
     ln -sfn $HOME/dotfiles/home/zsh $HOME/.config/zsh
     ln -sfn $HOME/dotfiles/home/zshrc $HOME/.zshrc
     ln -sfn $HOME/dotfiles/home/vimrc $HOME/.vimrc
