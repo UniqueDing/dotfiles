a2f7d64871a6402d2fd497b38302c9bb37b5a57f
uniqueding <uniqueding@uniqueding.xyz>
2026-09-25 00:27:52 +0800

    Refine Zsh completion and navigation keys

--- conf/zsh/plugins.zsh
+++ conf/zsh/plugins.zsh
@@ -6,8 +6,11 @@ if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
       https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 fi
 # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE} ]]; then
   source ${ZIM_HOME}/zimfw.zsh init -q
 fi
+
+# 某些全局配置已包装 compinit；在 Zim 再次初始化补全前移除其提示。
+functions[compinit]="${functions[compinit]#*$'\n'}"
 
 source ${ZIM_HOME}/init.zsh
--- conf/zsh/settings.zsh
+++ conf/zsh/settings.zsh
@@ -4,6 +4,11 @@ SAVEHIST=10000
 
 # emacs bindkey
 bindkey -e
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[3~" delete-char
+bindkey "\e[5~" up-line-or-history
+bindkey "\e[6~" down-line-or-history
 
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
